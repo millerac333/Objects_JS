36a2dbb1be35427bd30cbca639f589886e78a620
Aaron Miller <millerac333@gmail.com>
2018-05-29 11:30:28 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,81 @@
+var distict = {
+    tnDistrict: "5th"
+}
+
+const platforms = {
+    Taxes: "Cut Taxes",
+    Jobs: "Create Jobs",
+    Infrastructure: "Build roads",
+    Health_care: "Universal healh care",
+    Crime_and_enforcement: "Prevent crime by enforcing laws"
+}
+var urlDonation = {
+    siteURLd = "www.facebook.com"
+}
+var calendar = {
+    events: [
+        {
+            event: "picnic",
+            date: "4/5/18",
+            time: "12:00"
+        },
+        {
+            event: "speech",
+            date: "4/10/18",
+            time: "4:00"
+        },
+        {
+            event: "break",
+            date: "4/20/18",
+            time: "4:20"
+        }
+    ]
+}
+const volInfo = {
+    volunteer: [
+        {
+            Name: "Lebron James",
+            Address: "123 Cleveland St.",
+            Email: "kingjames@gmail.com",
+            Phone_number: "999-123-4567",
+            Availabiltiy: "never",
+            Activities: "none"
+        },
+        {
+            Name: "Dwayne Wade",
+            Address: "123 Miami St.",
+            Email: "wade@wade.com",
+            Availabiltiy: "always",
+            Activities: "all"
+        },
+        {
+            Name: "Kevin Love",
+            Address: "123 LA St.",
+            Email: "klove@nba.com",
+            Availabiltiy: "never",
+            Activities: "none"
+        }
+    ]
+}
+var bio = {
+    childhood: "hard childhood",
+    education: "Virginia Tech",
+    career: "This is my first run for office"
+}
+const Images = {
+    Head_shot: "https://pophealth.wisc.edu/sites/default/files/sanger_elizabeth.jpg",
+    Family: "https://www.nobelprize.org/nobel_prizes/medicine/laureates/2009/blackburn_autobio_2.jpg",
+    Constituents: "https://i.pinimg.com/originals/ce/37/44/ce37440a8780c12ef11f47ce8a3d09af.jpg"
+}
+var missionStatement = {
+    statement: "To be the very best and do the very best"
+}
+var urlRegister = {
+    siteURLr: "www.google.com"
+}
+
+
+
+
+
+
