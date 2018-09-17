583ad2c2d92a4103a7687b3f1833bb3faaa2ab15
Aaron Miller <millerac333@gmail.com>
2018-09-17 16:42:55 -0500

    fixed error and uncommented code and deleted commented code

--- main.js
+++ main.js
@@ -1,120 +1,112 @@
 ///2ND ATTEMPT AT EXCERCISE WHERE I PUT ALL DATA INTO ONE BIG OBJECT///
 let sangerElizabeth = {
-    tnCongressionalDistrict: "5th",
-    platform: {
-            taxes: "Cut Taxes",
-            jobs: "Create Jobs",
-            infrastructure: "Build roads",
-            healthCare: "Universal healh care",
-            CrimeAndEnforcement: "Prevent crime by enforcing laws"
-        },
-    urlDonation: "www.facebook.com",
-    calendar: {
-        events: [
-            {
-                event: "picnic",
-                date: "4/5/18",
-                time: "12:00"
-            },
-            {
-                event: "speech",
-                date: "4/10/18",
-                time: "4:00"
-            },
-            {
-                event: "break",
-                date: "4/20/18",
-                time: "4:20"
-            }
-        ]
-    },
-    volInfo: {
-        volunteer: [
-            {
-                Name: "Lebron James",
-                Address: "123 Cleveland St.",
-                Email: "kingjames@gmail.com",
-                Phone_number: "999-123-4567",
-                Availabiltiy: "never",
-                Activities: "none"
-            },
-            {
-                Name: "Dwayne Wade",
-                Address: "123 Miami St.",
-                Email: "wade@wade.com",
-                Availabiltiy: "always",
-                Activities: "all"
-            },
-            {
-                Name: "Kevin Love",
-                Address: "123 LA St.",
-                Email: "klove@nba.com",
-                Availabiltiy: "never",
-                Activities: "none"
-            }
-        ]
-    },
-    bio: {
-        childhood: "hard childhood",
-        education: "Virginia Tech",
-        career: "This is my first run for office"
-    },
-    sangerImages: {
-        Head_shot: "https://pophealth.wisc.edu/sites/default/files/sanger_elizabeth.jpg",
-        Family: "https://www.nobelprize.org/nobel_prizes/medicine/laureates/2009/blackburn_autobio_2.jpg",
-        Constituents: "https://i.pinimg.com/originals/ce/37/44/ce37440a8780c12ef11f47ce8a3d09af.jpg"
-    },
-    missionStatement: {
-        statement: "To be the very best and do the very best"
-    },
-    urlRegister: {
-        siteURLr: "www.google.com"
-    }
-}
+  tnCongressionalDistrict: "5th",
+  platform: {
+    taxes: "Cut Taxes",
+    jobs: "Create Jobs",
+    infrastructure: "Build roads",
+    healthCare: "Universal healh care",
+    CrimeAndEnforcement: "Prevent crime by enforcing laws"
+  },
+  urlDonation: "www.facebook.com",
+  calendar: {
+    events: [
+      {
+        event: "picnic",
+        date: "4/5/18",
+        time: "12:00"
+      },
+      {
+        event: "speech",
+        date: "4/10/18",
+        time: "4:00"
+      },
+      {
+        event: "break",
+        date: "4/20/18",
+        time: "4:20"
+      }
+    ]
+  },
+  volInfo: {
+    volunteer: [
+      {
+        Name: "Lebron James",
+        Address: "123 Cleveland St.",
+        Email: "kingjames@gmail.com",
+        Phone_number: "999-123-4567",
+        Availabiltiy: "never",
+        Activities: "none"
+      },
+      {
+        Name: "Dwayne Wade",
+        Address: "123 Miami St.",
+        Email: "wade@wade.com",
+        Availabiltiy: "always",
+        Activities: "all"
+      },
+      {
+        Name: "Kevin Love",
+        Address: "123 LA St.",
+        Email: "klove@nba.com",
+        Availabiltiy: "never",
+        Activities: "none"
+      }
+    ]
+  },
+  bio: {
+    childhood: "hard childhood",
+    education: "Virginia Tech",
+    career: "This is my first run for office"
+  },
+  sangerImages: {
+    Head_shot:
+      "https://pophealth.wisc.edu/sites/default/files/sanger_elizabeth.jpg",
+    Family:
+      "https://www.nobelprize.org/nobel_prizes/medicine/laureates/2009/blackburn_autobio_2.jpg",
+    Constituents:
+      "https://i.pinimg.com/originals/ce/37/44/ce37440a8780c12ef11f47ce8a3d09af.jpg"
+  },
+  missionStatement: {
+    statement: "To be the very best and do the very best"
+  },
+  urlRegister: {
+    siteURLr: "www.google.com"
+  }
+};
 
 // ------------------------------------------------------------------------------------------------------------------------
 
 ///CHALLENGE///
 //THIS FUNCTION WILL CHANGE THE STATE OF AN OBJECT LIKE THE ONE CREATED ABOVE//
-function changeObjectState (targetObject, keyReference, equalsValue) {
-    targetObject[keyReference] = equalsValue;
+function changeObjectState(targetObject, propertyReference, equalsValue) {
+  targetObject[propertyReference] = equalsValue;
 }
-// console.log(sangerElizabeth);
+console.log(sangerElizabeth);
 
-// changeObjectState(sangerElizabeth.platform, "jobs", "start a revolution");
-// console.log(sangerElizabeth);
+changeObjectState(sangerElizabeth.platform, "jobs", "start a revolution");
+console.log(sangerElizabeth);
 
-// changeObjectState(sangerElizabeth.platform, "taxes", "taxes? who needs taxes? REVOLT!");
-// console.log(sangerElizabeth);
+changeObjectState(
+  sangerElizabeth.platform,
+  "taxes",
+  "taxes? who needs taxes? REVOLT!"
+);
+console.log(sangerElizabeth);
 
-// changeObjectState(sangerElizabeth.missionStatement, "statement", "TAKE TO THE STREET MY FELLOW jedi AND RISE UP!");
-// console.log(sangerElizabeth);
+changeObjectState(
+  sangerElizabeth.missionStatement,
+  "statement",
+  "TAKE TO THE STREET MY FELLOW jedi AND RISE UP!"
+);
+console.log(sangerElizabeth);
 
-// changeObjectState(sangerElizabeth.volInfo.volunteer[1], "Avalibility", "never-DWADE is about to retire");
-// console.log(sangerElizabeth);
-
-// ------------------------------------------------------------------------------------------------------------------------
-
-//ADVANCED CHALLENGE//
-function createArticle() {
-    let newArticle = document.createElement("article");
-    newArticle.setAttribute("id", sangerElizabeth.tnCongressionalDistrict);
-    document.querySelector("body").appendChild(newArticle);
-    return newArticle;
-}
-createArticle();
-
-function createParagraph () {
-    let newParagraph = document.createElement("paragraph");
-    let newParagraphText = document.createTextNode = `${sangerElizabeth.platform}`
-    newParagraph.textContent = newParagraphText;
-    document.getElementById("5th").appendChild(newParagraph);
-    return newParagraph;
-}
-
-createParagraph(sangerElizabeth);
-
-// ------------------------------------------------------------------------------------------------------------------------
+changeObjectState(
+  sangerElizabeth.volInfo.volunteer[1],
+  "Avalibility",
+  "never-DWADE is about to retire"
+);
+console.log(sangerElizabeth);
 
 ///FIRST ATTEMPT AT EXCERCISE///
 // var district = {
@@ -123,7 +115,7 @@ createParagraph(sangerElizabeth);
 // function nameDistrict(numberOfDistrict) {
 //     district.tnDistrict = numberOfDistrict;
 //     console.log(numberOfDistrict);
-// }   
+// }
 // console.log(district.tnDistrict);
 // nameDistrict("Fifth");
 // console.log(district.tnDistrict);
